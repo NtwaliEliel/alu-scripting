c3bc3ab51edc112474a5dbd2a80efc07d6434dda
Diverse2 <ntwalie83@gamil.com>
2024-07-02 15:18:57 +0200

    special init

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Importing required modules."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """number_of_subscribers returns number of subscribers."""
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Bot/1.0)'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
+
+# Example usage:
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))]
+        
