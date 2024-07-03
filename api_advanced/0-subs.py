d4523b9852ce185107460dc631a12c16da0f8f8e
Diverse2 <ntwalie83@gamil.com>
2024-07-03 14:13:03 +0200

    error4

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,3 +8,14 @@ import requests
 def number_of_subscribers(subreddit):
     """number_of_subscribers returns
     number of subscribers by searching reddit for a subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.exceptions.ConnectionError:
+        return 0
