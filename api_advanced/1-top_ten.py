6e44fcd8d7247eb34739e833b3103b6a668d514b
Diverse2 <ntwalie83@gamil.com>
2024-07-03 14:28:50 +0200

    editing file 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,6 +7,7 @@ the titles of the first 10 hot posts for a given subreddit.
 
 import requests
 
+
 def top_ten(subreddit):
     """
     Queries the Reddit API and prints the titles of the first 10 hot posts
@@ -15,7 +16,7 @@ def top_ten(subreddit):
     Args:
         subreddit (str): The name of the subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (compatible; Bot/1.0)'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
