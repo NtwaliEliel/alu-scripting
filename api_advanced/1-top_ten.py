543dd152e3ba2bb4337c824a488f643eb81c323a
Diverse2 <ntwalie83@gamil.com>
2024-07-03 13:33:53 +0200

    added file 1

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit. If the subreddit is invalid, prints None.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Bot/1.0)'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
