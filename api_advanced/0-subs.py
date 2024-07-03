0fa6044424e87eab82ea895c258cf2d9bf21ca68
Diverse2 <ntwalie83@gamil.com>
2024-07-03 14:23:19 +0200

    changing file 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """number_of_subscribers returns
     number of subscribers by searching reddit for a subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Bot/1.0)'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
@@ -18,5 +18,5 @@ def number_of_subscribers(subreddit):
             return data['data']['subscribers']
         else:
             return 0
-    except requests.exceptions.ConnectionError:
+    except requests.RequestException:
         return 0
